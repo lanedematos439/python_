be208aeb1b2918082b3e7b7e228b8b23bf4ad845
lanedematos439 <lanedematos439@gmail.com>
2025-09-26 10:25:45 -0300

    atualizacao

--- Carro.py
+++ Carro.py
@@ -1,61 +1,39 @@
-# class Carro:
-#     def __init__(self , marca , modelo , ano , cor):
-#         self.marca = marca
-#         self.modelo = modelo
-#         self.ano = ano
-#         self.cor = cor
-#         self.velocidade = 0
-
-#     def acelerar(self, valor):
-#         self.velocidade += valor
-#         print(f"{self.modelo} acelerou para {self.velocidade} km/h!")
-
-#     def frear(self, valor):
-#         self.velocidade -= valor
-#         if self.velocidade < 0:
-#             self.velocidade = 0
-#             print(f"{self.modelo} reduziu para {self.velocidade} km/h.")
-    
-#     def detalhes(self):
-#         return (f"{self.marca} {self.modelo} ({self.ano}) - "
-#         f"cor: {self.cor}, velocidade: {self.velocidade} km/h")
-
-#         carro1 = Carro("Toyota", "Corolla", 2020, "preto")
-#         carro2 = Carro("Honda", "Civic", "2019", "Vermelho")
-
-#         print(carro1.detalhes())
-#         print(carro2.detalhes())
-
-#         carro1.acelerar(50)
-#         carro2.acelerar(30)
-
-#         carro1.frear(20)
-#         carro2.frear(15)
-
-#         print(carro1.detalhes())
-#         print(carro2.detalhes())
-class Moto:
-    def __init__(self, marca, modelo, ano, cor):
+class Carro:
+    def __init__(self , marca , modelo , ano , cor):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
-        self.cor = cor 
+        self.cor = cor
         self.velocidade = 0
 
     def acelerar(self, valor):
-        self.velociade += valor
-        print(f"{self.modelo} acelerou para {self.velocidade} km/!")
-    
+        self.velocidade += valor
+        print(f"{self.modelo} acelerou para {self.velocidade} km/h!")
+
     def frear(self, valor):
         self.velocidade -= valor
         if self.velocidade < 0:
             self.velocidade = 0
-        print(f"{self.marca} reduziu {self.velocidade} km/h.")
-
+            print(f"{self.modelo} reduziu para {self.velocidade} km/h.")
+    
     def detalhes(self):
         return (f"{self.marca} {self.modelo} ({self.ano}) - "
         f"cor: {self.cor}, velocidade: {self.velocidade} km/h")
 
+        carro1 = Carro("Toyota", "Corolla", 2020, "preto")
+        carro2 = Carro("Honda", "Civic", "2019", "Vermelho")
+
+        print(carro1.detalhes())
+        print(carro2.detalhes())
+
+        carro1.acelerar(50)
+        carro2.acelerar(30)
+
+        carro1.frear(20)
+        carro2.frear(15)
+
+        print(carro1.detalhes())
+        print(carro2.detalhes())
 
 
 
---
+++ Moto.py
@@ -0,0 +1,36 @@
+class Moto:
+    def __init__(self, marca, modelo, ano, cor):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor 
+        self.velocidade = 0
+
+    def acelerar(self, valor):
+        self.velociade += valor
+        print(f"{self.modelo} acelerou para {self.velocidade} km/!")
+    
+    def frear(self, valor):
+        self.velocidade -= valor
+        if self.velocidade < 0:
+            self.velocidade = 0
+            print(f"{self.marca} reduziu {self.velocidade} km/h.")
+
+    def detalhes(self):
+        return (f"{self.marca} {self.modelo} ({self.ano}) - "
+        f"cor: {self.cor}, velocidade: {self.velocidade} km/h")
+
+        moto1 = Moto("Yamaha", "trail XTZ", 2020, "azul")
+        moto2 = Moto("Honda", "Civic", "2019", "Amarela")
+
+        print(moto1.detalhes())
+        print(moto2.detalhes())
+
+        moto1.acelerar(50)
+        moto2.acelerar(30)
+
+        moto1.frear(20)
+        moto2.frear(15)
+
+        print(moto1.detalhes())
+        print(moto2.detalhes())
